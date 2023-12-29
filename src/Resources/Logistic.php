e4747886aed58753816001d5d7c0b47f5ca32347
Jin <j@shopar.dev>
2023-12-29 11:16:41 +0700

    complete Logistic & Payment API

--- src/Resources/Logistic.php
+++ src/Resources/Logistic.php
@@ -16,6 +16,26 @@ use EcomPHP\Shopee\Resource;
 class Logistic extends Resource
 {
     /**
+     * API: v2.logistics.get_shipping_parameter
+     * Use this api to get the parameter "info_needed" from the response to check if the order has pickup or dropoff or no integrate options.
+     * This api will also return the addresses and pickup time id options for the pickup method.
+     * For dropoff, it can return branch id, sender real name etc, depending on the 3PL requirements.
+     *
+     * @param  string  $order_sn
+     * @param  string  $package_number
+     * @return array|mixed
+     */
+    public function getShippingParameter($order_sn, $package_number = '')
+    {
+        return $this->call('GET', 'logistics/get_shipping_parameter', [
+            RequestOptions::QUERY => [
+                'order_sn' => $order_sn,
+                'package_number' => $package_number,
+            ],
+        ]);
+    }
+
+    /**
      * API: v2.logistics.get_tracking_number
      * Use this api to get tracking_number when you have shipped order.
      *
@@ -31,48 +51,262 @@ class Logistic extends Resource
         ]);
     }
 
-    public function getShippingDocumentResult($order_sn_list = [], $params = [])
+    /**
+     * API: v2.logistics.ship_order
+     * Use this api to initiate logistics including arrange pickup, dropoff or shipment for non-integrated logistic channels.
+     * Should call v2.logistics.get_shipping_parameter to fetch all required param first.
+     * It's recommended to initiate logistics one hour after the orders were placed since there is one-hour window buyer can cancel any order without request to seller.
+     *
+     * @param  string $order_sn
+     * @param  string $package_number
+     * @param  array $pickup
+     * @param  array $dropoff
+     * @param  array $non_integrated
+     * @return array|mixed
+     */
+    public function shipOrder($order_sn, $package_number = '', $pickup = [], $dropoff = [], $non_integrated = [])
     {
-        $params = array_merge([
-            'order_list' => array_map(function ($order_sn) {
-                return [
-                    'order_sn' => $order_sn,
-                ];
-            }, $order_sn_list)
-        ], $params);
+        $params = [
+            'order_sn' => $order_sn,
+            'package_number' => $package_number,
+            'pickup' => $pickup,
+            'dropoff' => $dropoff,
+            'non_integrated' => $non_integrated,
+        ];
 
-        return $this->call('POST', 'logistics/get_shipping_document_result', [
+        return $this->call('POST', 'logistics/ship_order', [
             RequestOptions::JSON => $params,
         ]);
     }
 
-    public function downloadShippingDocument($order_sn_list = [], $params = [])
+    /**
+     * API: v2.logistics.update_shipping_order
+     * For pickup method only, use this api to update pickup address and pickup time for orders in "RETRY_SHIP" status.
+     *
+     * @param  string  $order_sn
+     * @param  string  $package_number
+     * @param  array  $pickup
+     * @return array|mixed
+     */
+    public function updateShippingOrder($order_sn, $package_number = '', $pickup = [])
     {
-        $params = array_merge([
-            'order_list' => array_map(function ($order_sn) {
-                return [
-                    'order_sn' => $order_sn,
-                ];
-            }, $order_sn_list)
-        ], $params);
+        $params = [
+            'order_sn' => $order_sn,
+            'package_number' => $package_number,
+            'pickup' => $pickup,
+        ];
 
-        return $this->call('POST', 'logistics/download_shipping_document', [
+        return $this->call('POST', 'logistics/update_shipping_order', [
             RequestOptions::JSON => $params,
         ]);
     }
 
-    public function createShippingDocument($order_sn_list = [], $params = [])
+    /**
+     * API: v2.logistics.get_shipping_document_parameter
+     * Use this api to get the selectable shipping_document_type and suggested shipping_document_type.
+     *
+     * @param  array{order_sn: string, package_number: string}  $order_list
+     * @return array|mixed
+     */
+    public function getShippingDocumentParameter($order_list)
     {
-        $params = array_merge([
-            'order_list' => array_map(function ($order_sn) {
-                return [
-                    'order_sn' => $order_sn,
-                ];
-            }, $order_sn_list)
-        ], $params);
+        return $this->call('POST', 'logistics/get_shipping_document_parameter', [
+            RequestOptions::JSON => [
+                'order_list' => $order_list,
+            ],
+        ]);
+    }
 
+    /**
+     * API: v2.logistics.create_shipping_document
+     * Use this api to create shipping document task for each order or package and this API is only available after retrieving the tracking number.
+     *
+     * @param array{order_sn: string, package_number: string, tracking_number: string, shipping_document_type: string} $order_list
+     * @return array|mixed|string
+     */
+    public function createShippingDocument($order_list = [])
+    {
         return $this->call('POST', 'logistics/create_shipping_document', [
-            RequestOptions::JSON => $params,
+            RequestOptions::JSON => [
+                'order_list' => $order_list,
+            ],
+        ]);
+    }
+
+    /**
+     * API: v2.logistics.get_shipping_document_result
+     * Use this api to retrieve the status of the shipping document task. Document will be available for download only after the status change to 'READY'.
+     *
+     * @param  array{order_sn: string, package_number: string, shipping_document_type: string}  $order_list
+     * @return array|mixed
+     */
+    public function getShippingDocumentResult($order_list = [])
+    {
+        return $this->call('POST', 'logistics/get_shipping_document_result', [
+            RequestOptions::JSON => [
+                'order_list' => $order_list,
+            ],
+        ]);
+    }
+
+    /**
+     * API: v2.logistics.download_shipping_document
+     * Use this api to download shipping_document.
+     * You have to call v2.logistics.create_shipping_document to create a new shipping document task first and call v2.logistics.get_shipping_document_result to get the task status second.
+     * If the task is READY, you can download this shipping document.
+     *
+     * @param  array{order_sn: string, package_number: string}  $order_list
+     * @param  string $shipping_document_type
+     * @return array|mixed
+     */
+    public function downloadShippingDocument($order_list = [], $shipping_document_type = '')
+    {
+        return $this->call('POST', 'logistics/download_shipping_document', [
+            RequestOptions::JSON => [
+                'order_list' => $order_list,
+                'shipping_document_type' => $shipping_document_type,
+            ],
+        ]);
+    }
+
+    /**
+     * API: v2.logistics.get_tracking_info
+     * Use this api to get the logistics tracking information of an order.
+     *
+     * @param  string  $order_sn
+     * @param  string  $package_number
+     * @return array|mixed
+     */
+    public function getTrackingInfo($order_sn, $package_number = '')
+    {
+        return $this->call('GET', 'logistics/get_tracking_info', [
+            RequestOptions::QUERY => [
+                'order_sn' => $order_sn,
+                'package_number' => $package_number,
+            ],
+        ]);
+    }
+
+    /**
+     * API: v2.logistics.get_address_list
+     * For integrated logistics channel, use this call to get pickup address for pickup mode order.
+     *
+     * @return array|mixed
+     */
+    public function getAddressList()
+    {
+        return $this->call('GET', 'logistics/get_address_list');
+    }
+
+    /**
+     * API: v2.logistics.set_address_config
+     * Use this API to set address config of your shop.
+     *
+     * @param array{address_id: int, address: string, address_type: array} $address_type_config
+     * @param bool $show_pickup_address
+     * @return array|mixed
+     */
+    public function setAddressConfig($address_type_config = [], $show_pickup_address = true)
+    {
+        return $this->call('POST', 'logistics/set_address_config', [
+            RequestOptions::JSON => [
+                'address_type_config' => $address_type_config,
+                'show_pickup_address' => $show_pickup_address,
+            ],
+        ]);
+    }
+
+    /**
+     * API: v2.logistics.delete_address
+     * Use this api to delete address.
+     *
+     * @param  int  $address_id
+     * @return array|mixed
+     */
+    public function deleteAddress($address_id)
+    {
+        return $this->call('POST', 'logistics/delete_address', [
+            RequestOptions::JSON => [
+                'address_id' => $address_id,
+            ],
+        ]);
+    }
+
+    /**
+     * API: v2.logistics.get_channel_list
+     * Use this api to get branch list.
+     *
+     * @return array|mixed
+     */
+    public function getChannelList()
+    {
+        return $this->call('GET', 'logistics/get_channel_list');
+    }
+
+    /**
+     * API: v2.logistics.update_channel
+     * Use this api to update shop level logistics channel's configuration.
+     *
+     * @param  int  $logistics_channel_id
+     * @param  bool $enabled
+     * @param  bool $cod_enabled
+     * @return array|mixed
+     */
+    public function updateChannel($logistics_channel_id, $enabled = true, $cod_enabled = true)
+    {
+        return $this->call('POST', 'logistics/update_channel', [
+            RequestOptions::JSON => [
+                'logistics_channel_id' => $logistics_channel_id,
+                'enabled' => $enabled,
+                'cod_enabled' => $cod_enabled,
+            ],
+        ]);
+    }
+
+    /**
+     * API: v2.logistics.batch_ship_order
+     * Use this api to batch initiate logistics including arrange pickup, dropoff or shipment for non-integrated logistic channels.
+     * Should call v2.logistics.get_shipping_parameter to fetch all required param first.
+     * It's recommended to initiate logistics one hour after the orders were placed since there is one-hour window buyer can cancel any order without request to seller.Only channel 90003 - Padrão in Brazil has the permission of this API.
+     *
+     * @param  array{order_sn: string, package_number: string}  $order_list
+     * @param  array  $pickup
+     * @param  array  $dropoff
+     * @param  array  $non_integrated
+     * @return array|mixed
+     */
+    public function batchShipOrder($order_list = [], $pickup = [], $dropoff = [], $non_integrated = [])
+    {
+        return $this->call('POST', 'logistics/batch_ship_order', [
+            RequestOptions::JSON => [
+                'order_list' => $order_list,
+                'pickup' => $pickup,
+                'dropoff' => $dropoff,
+                'non_integrated' => $non_integrated,
+            ],
+        ]);
+    }
+
+    /**
+     * API: v2.logistics.get_shipping_document_data_info
+     * Use this api to fetch the logistics information of an order, these info can be used for airwaybill printing.
+     * Dedicated for crossborder SLS order airwaybill.
+     * May not be applicable for local channel airwaybill.
+     * Besides, this api supports returning personal info as images.
+     *
+     * @param  string $order_sn
+     * @param  string $package_number
+     * @param  array $recipient_address_info
+     * @return array|mixed
+     */
+    public function getShippingDocumentDataInfo($order_sn, $package_number = '', $recipient_address_info = [])
+    {
+        return $this->call('POST', 'logistics/get_shipping_document_data_info', [
+            RequestOptions::JSON => [
+                'order_sn' => $order_sn,
+                'package_number' => $package_number,
+                'recipient_address_info' => $recipient_address_info,
+            ],
         ]);
     }
 }
--- src/Resources/Payment.php
+++ src/Resources/Payment.php
@@ -17,10 +17,10 @@ class Payment extends Resource
 {
     /**
      * API: v2.payment.get_escrow_detail
-     * Use this api to get escrow detail of order
+     * Use this API to fetch the accounting detail of order.
      *
      * @param $order_sn
-     * @return array|mixed|string
+     * @return array|mixed
      */
     public function getEscrowDetail($order_sn)
     {
@@ -28,6 +28,165 @@ class Payment extends Resource
             RequestOptions::QUERY => [
                 'order_sn' => $order_sn,
             ],
+        ]);
+    }
+
+    /**
+     * API: v2.payment.set_shop_installment_status
+     * Sets the staging capability of shop level.
+     *
+     * @param int $installment_status
+     * @return array|mixed
+     */
+    public function setShopInstallmentStatus($installment_status)
+    {
+        return $this->call('POST', 'payment/set_shop_installment_status', [
+            RequestOptions::JSON => [
+                'installment_status' => $installment_status,
+            ],
+        ]);
+    }
+
+    /**
+     * API: v2.payment.get_shop_installment_status
+     * Get the installment state of shop.
+     *
+     * @return array|mixed
+     */
+    public function getShopInstallmentStatus()
+    {
+        return $this->call('GET', 'payment/get_shop_installment_status');
+    }
+
+    /**
+     * API: v2.payment.get_payout_detail
+     * This API is applicable for Cross Border (CB) sellers only to get the shop's payout data, such as the payout amount, currency, FX rate, the payout's associated order income and adjustment records etc.
+     *
+     * @param int $page_size
+     * @param int $page_no
+     * @param int $time_from
+     * @param int $time_to
+     * @return array|mixed
+     */
+    public function getPayoutDetail($page_size, $page_no, $time_from, $time_to)
+    {
+        return $this->call('GET', 'payment/get_payout_detail', [
+            RequestOptions::QUERY => [
+                'page_size' => $page_size,
+                'page_no' => $page_no,
+                'payout_time_from' => $time_from,
+                'payout_time_to' => $time_to,
+            ],
+        ]);
+    }
+
+    /**
+     * API: v2.payment.set_item_installment_status
+     * Set item installment.Only for TH、TW.
+     *
+     * @param int[] $item_id_list
+     * @param int[] $tenure_list
+     * @param bool $participate_plan_ahora
+     * @return array|mixed
+     */
+    public function setItemInstallmentStatus($item_id_list, $tenure_list, $participate_plan_ahora = false)
+    {
+        return $this->call('POST', 'payment/set_item_installment_status', [
+            RequestOptions::JSON => [
+                'item_id_list' => $item_id_list,
+                'tenure_list' => $tenure_list,
+                'participate_plan_ahora' => $participate_plan_ahora,
+            ],
+        ]);
+    }
+
+    /**
+     * API: v2.payment.get_item_installment_status
+     * Get item installment tenures.Only for TH、TW.*
+     * @param int[] $item_id_list
+     * @return array|mixed
+     */
+    public function getItemInstallmentStatus($item_id_list)
+    {
+        return $this->call('POST', 'payment/get_item_installment_status', [
+            RequestOptions::JSON => [
+                'item_id_list' => $item_id_list,
+            ],
+        ]);
+    }
+
+    /**
+     * API: v2.payment.get_payment_method_list
+     * Obtain payment method (no authentication required)
+     *
+     * @return array|mixed
+     */
+    public function getPaymentMethodList()
+    {
+        return $this->call('GET', 'payment/get_payment_method_list');
+    }
+
+    /**
+     * API: v2.payment.get_wallet_transaction_list
+     * Get transaction list
+     *
+     * @param int $page_size
+     * @param int $page_no
+     * @param int $create_time_from
+     * @param int $create_time_to
+     * @param string $wallet_type
+     * @param string $transaction_type
+     * @return array|mixed
+     */
+    public function getWalletTransactionList($page_size, $page_no, $create_time_from, $create_time_to, $wallet_type = null, $transaction_type = null)
+    {
+        $params = [
+            'page_size' => $page_size,
+            'page_no' => $page_no,
+            'create_time_from' => $create_time_from,
+            'create_time_to' => $create_time_to,
+        ];
+
+        if ($wallet_type) {
+            $params['wallet_type'] = $wallet_type;
+        }
+
+        if ($transaction_type) {
+            $params['transaction_type'] = $transaction_type;
+        }
+
+        return $this->call('GET', 'payment/get_wallet_transaction_list', [
+            RequestOptions::QUERY => $params,
+        ]);
+    }
+
+    /**
+     * API: v2.payment.get_escrow_list
+     * Get transaction details
+     *
+     * @param int $release_time_from
+     * @param int $release_time_to
+     * @param int $page_size
+     * @param int $page_no
+     * @return array|mixed
+     */
+    public function getEscrowList($release_time_from, $release_time_to, $page_size = null, $page_no = null)
+    {
+        $params = [
+            'release_time_from' => $release_time_from,
+            'release_time_to' => $release_time_to,
+        ];
+
+        if ($page_size) {
+            $params['page_size'] = $page_size;
+        }
+
+        if ($page_no) {
+            $params['page_no'] = $page_no;
+        }
+
+        return $this->call('GET', 'payment/get_escrow_list', [
+            RequestOptions::QUERY => $params,
         ]);
     }
 }
