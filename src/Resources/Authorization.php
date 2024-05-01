35168d1120b8dbff132510653debf3ad857c6c6a
nVuln <j@shopar.dev>
2024-05-01 15:15:29 +0700

    fix: add some Public api to Authorization

--- src/Resources/Authorization.php
+++ src/Resources/Authorization.php
@@ -16,10 +16,33 @@ use EcomPHP\Shopee\Resource;
 class Authorization extends Resource
 {
     /**
-     * @param $code
-     * @param $shop_id
-     * @param $partner_id
-     * @return mixed
+     * API: v2.public.get_shops_by_partner
+     */
+    public function getShopsByPartner($params = [])
+    {
+        return $this->call('GET', 'public/get_shops_by_partner', [
+            RequestOptions::QUERY => array_merge([
+                'page_no' => 1,
+                'page_size' => 100,
+            ], $params),
+        ]);
+    }
+
+    /**
+     * API: v2.public.get_merchants_by_partner
+     */
+    public function getMerchantsByPartner($params = [])
+    {
+        return $this->call('GET', 'public/get_merchants_by_partner', [
+            RequestOptions::QUERY => array_merge([
+                'page_no' => 1,
+                'page_size' => 100,
+            ], $params),
+        ]);
+    }
+
+    /**
+     * API: v2.public.get_access_token
      */
     public function getToken($code, $shop_id, $partner_id)
     {
@@ -33,10 +56,7 @@ class Authorization extends Resource
     }
 
     /**
-     * @param $refresh_token
-     * @param $shop_id
-     * @param $partner_id
-     * @return mixed
+     * API: v2.public.refresh_access_token
      */
     public function refreshNewToken($refresh_token, $shop_id, $partner_id)
     {
@@ -47,5 +67,38 @@ class Authorization extends Resource
                 'partner_id' => intval($partner_id),
             ]
         ]);
+    }
+
+    /**
+     * API: v2.public.get_token_by_resend_code
+     */
+    public function getTokenByResendCode($resend_code)
+    {
+        return $this->call('POST', 'public/get_token_by_resend_code', [
+            RequestOptions::JSON => [
+                'resend_code' => $resend_code,
+            ]
+        ]);
+    }
+
+    /**
+     * API: v2.public.get_refresh_token_by_upgrade_code
+     */
+    public function getRefreshTokenByUpgradeCode($upgrade_code, $shop_id_list)
+    {
+        return $this->call('POST', 'public/get_refresh_token_by_upgrade_code', [
+            RequestOptions::JSON => [
+                'upgrade_code' => $upgrade_code,
+                'shop_id_list' => $shop_id_list,
+            ]
+        ]);
+    }
+
+    /**
+     * API: v2.public.get_shopee_ip_ranges
+     */
+    public function getShopeeIpRanges()
+    {
+        return $this->call('GET', 'public/get_shopee_ip_ranges');
     }
 }
