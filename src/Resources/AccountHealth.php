c96d2baa78f69991a0880d91105438391f785cf6
nVuln <j@sax.vn>
2025-03-01 14:03:11 +0700

    add: some new api on AccountHealth

--- src/Resources/AccountHealth.php
+++ src/Resources/AccountHealth.php
@@ -11,6 +11,7 @@
 namespace EcomPHP\Shopee\Resources;
 
 use EcomPHP\Shopee\Resource;
+use GuzzleHttp\RequestOptions;
 
 class AccountHealth extends Resource
 {
@@ -28,5 +29,73 @@ class AccountHealth extends Resource
     public function shopPenalty()
     {
         return $this->call('GET', 'account_health/shop_penalty');
+    }
+
+    /**
+     * v2.account_health.get_metric_source_detail
+     */
+    public function getMetricSourceDetail($metric_id, $page_no = 1, $page_size = 100)
+    {
+        return $this->call('GET', 'account_health/get_metric_source_detail', [
+            RequestOptions::QUERY => [
+                'metric_id' => $metric_id,
+                'page_no' => $page_no,
+                'page_size' => $page_size,
+            ]
+        ]);
+    }
+
+    /**
+     * v2.account_health.get_penalty_point_history
+     */
+    public function getPenaltyPointHistory($violation_type = null, $page_no = 1, $page_size = 100)
+    {
+        return $this->call('GET', 'account_health/get_penalty_point_history', [
+            RequestOptions::QUERY => array_filter([
+                'page_no' => $page_no,
+                'page_size' => $page_size,
+                'violation_type' => $violation_type,
+            ])
+        ]);
+    }
+
+    /**
+     * v2.account_health.get_punishment_history
+     */
+    public function getPunishmentHistory($punishment_status, $page_no = 1, $page_size = 100)
+    {
+        return $this->call('GET', 'account_health/get_punishment_history', [
+            RequestOptions::QUERY => array_filter([
+                'page_no' => $page_no,
+                'page_size' => $page_size,
+                'punishment_status' => $punishment_status,
+            ])
+        ]);
+    }
+
+    /**
+     * v2.account_health.get_listings_with_issues
+     */
+    public function getListingsWithIssues($page_no = 1, $page_size = 100)
+    {
+        return $this->call('GET', 'account_health/get_listings_with_issues', [
+            RequestOptions::QUERY => array_filter([
+                'page_no' => $page_no,
+                'page_size' => $page_size,
+            ])
+        ]);
+    }
+
+    /**
+     * v2.account_health.get_late_orders
+     */
+    public function getLateOrders($page_no = 1, $page_size = 100)
+    {
+        return $this->call('GET', 'account_health/get_late_orders', [
+            RequestOptions::QUERY => array_filter([
+                'page_no' => $page_no,
+                'page_size' => $page_size,
+            ])
+        ]);
     }
 }
